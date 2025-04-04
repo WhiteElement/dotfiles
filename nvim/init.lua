edb5670eb492aef57b52131c5958cebb719c4006
Manuel Braunwald <78162213+WhiteElement@users.noreply.github.com>
2025-04-04 22:11:32 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -656,20 +656,30 @@ require("lazy").setup({
 			})
 		end,
 	},
-	-- {
-	--   'chriskempson/base16-vim',
-	--   priority = 1000,
-	--   init = function()
-	--     vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
-	--   end,
-	-- },
-	{
-		"EdenEast/nightfox.nvim",
-		priority = 1000,
-		init = function()
-			vim.cmd.colorscheme("carbonfox")
-		end,
-	},
+	  -- lua/plugins/rose-pine.lua
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      vim.cmd 'colorscheme rose-pine'
+      vim.cmd 'highlight Normal guibg=black'
+    end,
+  },
+  -- {
+  --   'chriskempson/base16-vim',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
+  --   end,
+  -- },
+  -- {
+  --   'EdenEast/nightfox.nvim',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.cmd.colorscheme 'carbonfox'
+  --   end,
+  -- },
+
 	-- Highlight todo, notes, etc in comments
 	{
 		"folke/todo-comments.nvim",
