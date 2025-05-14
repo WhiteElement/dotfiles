43840307cfd2db047e86c3b9f6cddaf9184580c6
Manuel Braunwald <manuelbrusche@gmail.com>
2025-05-14 20:28:21 +0200

    updated wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,87 +1,73 @@
--- Import wezterm module
 local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
 
-return {
-	-- default_prog = { "cmd", "for /f 'tokens=*' %i in ('opam env') do @%i" },
-	color_scheme = "Catppuccin Macchiato",
-	-- font = wezterm.font("JetBrains Mono NL"),
-	font_size = 16,
+-- Startlocation
+config.default_cwd = "C://Entwicklung"
 
-	-- Define leader key
-	leader = { key = "w", mods = "ALT", timeout_milliseconds = 1000 },
+-- Change Frontend -> OpenGL had graphics artefacts when not full screen
+config.front_end = "WebGpu"
 
-	keys = {
-		{ key = "\\", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+-- Set leader key to Ctrl+a, like tmux
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 
-		{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+-- Keybindings to mimic tmux
+config.keys = {
+	-- Scrolling
+	{ key = "PageUp", action = act.ScrollByPage(-1) },
+	{ key = "PageDown", action = act.ScrollByPage(1) },
 
-		{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
-		{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
-		{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
-		{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	-- Searching
+	{ key = "f", mods = "CTRL", action = wezterm.action.Search({ CaseInSensitiveString = "" }) },
 
-		{ key = "LeftArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Left", 7 }) },
-		{ key = "RightArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Right", 7 }) },
-		{ key = "UpArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Up", 4 }) },
-		{ key = "DownArrow", mods = "LEADER", action = wezterm.action.AdjustPaneSize({ "Down", 4 }) },
+	-- Pane splitting
+	{ key = "_", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) }, -- Ctrl+a | for vertical split
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) }, -- Ctrl+a - for horizontal split
 
-		{ key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("DefaultDomain") },
+	-- Pane navigation
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") }, -- Ctrl+a h
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") }, -- Ctrl+a j
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") }, -- Ctrl+a k
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") }, -- Ctrl+a l
+	{ key = "q", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
 
-		{
-			key = "r",
-			mods = "LEADER",
-			action = wezterm.action.PromptInputLine({
-				description = "Enter new tab name",
-				action = wezterm.action_callback(function(window, pane, line)
-					if line then
-						window:active_tab():set_title(line)
-					end
-				end),
-			}),
-		},
+	-- Pane resizing
+	{ key = "H", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Left", 5 }) }, -- Ctrl+a H
+	{ key = "J", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Down", 5 }) }, -- Ctrl+a J
+	{ key = "K", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Up", 5 }) }, -- Ctrl+a K
+	{ key = "L", mods = "LEADER|SHIFT", action = act.AdjustPaneSize({ "Right", 5 }) }, -- Ctrl+a L
 
-		{ key = "w", mods = "LEADER", action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
-		{ key = "n", mods = "LEADER", action = wezterm.action.SwitchWorkspaceRelative(1) },
+	-- Tab management
+	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") }, -- Ctrl+a c for new tab
+	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) }, -- Ctrl+a n for next tab
+	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) }, -- Ctrl+a p for previous tab
+	{ key = "Q", mods = "LEADER|SHIFT", action = act.CloseCurrentTab({ confirm = true }) }, -- Ctrl+a & to close tab
 
-		{ key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
-		{ key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
-		{ key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
-		{ key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
-		{ key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
-		{ key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
-		{ key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
-		{ key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
-		{ key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
-		{ key = "0", mods = "LEADER", action = wezterm.action.ActivateTab(9) },
-	},
-
-	-- tab bar
-	hide_tab_bar_if_only_one_tab = false,
-	tab_bar_at_bottom = true,
-	use_fancy_tab_bar = false,
-	tab_and_split_indices_are_zero_based = false,
-
-	-- tmux status
-	wezterm.on("update-right-status", function(window, _)
-		local SOLID_LEFT_ARROW = ""
-		local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-		local prefix = ""
-
-		if window:leader_is_active() then
-			prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-			SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-		end
-
-		if window:active_tab():tab_id() ~= 0 then
-			ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-		end -- arrow color based on if tab is first pane
-
-		window:set_left_status(wezterm.format({
-			{ Background = { Color = "#b7bdf8" } },
-			{ Text = prefix },
-			ARROW_FOREGROUND,
-			{ Text = SOLID_LEFT_ARROW },
-		}))
-	end),
+	-- Workspace (tmux session) switching
+	{ key = "s", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) }, -- Ctrl+a s to list workspaces
+	{
+		key = "W",
+		mods = "LEADER|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter name for new workspace",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:perform_action(act.SwitchToWorkspace({ name = line }), pane)
+				end
+			end),
+		}),
+	}, -- Ctrl+a W to create/switch workspace
 }
+
+-- Basic visual settings to match tmux aesthetics
+config.font_size = 14
+config.window_decorations = "TITLE | RESIZE"
+config.color_scheme = "Gruvbox Dark"
+
+-- Tab Bar
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+
+return config
